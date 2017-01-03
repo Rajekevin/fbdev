8c87ca9fd1482c2ed863b348da243a438dfcfb60
Aktane <antoine.renault.mmi@gmail.com>
2017-01-03 16:04:06 +0100

    Add load more methods

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,9 +1,9 @@
-require('./bootstrap');
+require('./bootstrap')
 
-Vue.component('app', require('./components/App.vue'));
+Vue.component('app', require('./components/App.vue'))
 
 window.onload = function () {
 	const app = new Vue({
 		el: '#app'
-	});
+	})
 }
--- resources/assets/js/components/Grid.vue
+++ resources/assets/js/components/Grid.vue
@@ -1,6 +1,16 @@
 <template>
 	<div class="images_container">
-		<item index="1"></item>
+		<div v-if="images.length <= maxNumberOfImageByChunk">
+			<item v-for="(item, index) in images" :index="item.index"></item>
+		</div>
+		<div v-else>
+			<p>CurrentChunk - {{ currentChunk }}</p>
+			<item v-for="(item, index) in chunkImages" :index="item.index"></item>
+			<p v-if="images.length > indexOfActualChunk">
+				<button @click="loadMore()">Load more</button>
+			</p>
+		</div>
+		
 	</div>
 </template>
 
@@ -9,11 +19,47 @@ import Item from "./Item.vue"
 export default {
 	data: function () {
 		return {
-			repeat: '3'
+			repeat: '3',
+			maxNumberOfImageByChunk: 12,
+			currentChunk: 1,
+			numberOfTotalImage: 9,
+			images: [
+				{ index: 1 },
+				{ index: 2 },
+				{ index: 3 },
+				{ index: 1 },
+				{ index: 2 },
+				{ index: 3 },
+				{ index: 1 },
+				{ index: 2 },
+				{ index: 3 },
+				{ index: 1 },
+				{ index: 2 },
+				{ index: 3 },
+				{ index: 1 },
+				{ index: 2 },
+				{ index: 3 },
+				{ index: 1 },
+				{ index: 2 },
+				{ index: 3 }
+			]
+		}
+	},
+	computed: {
+	    indexOfActualChunk: function () {
+	    	return this.currentChunk * this.maxNumberOfImageByChunk
+	    },
+	    chunkImages: function () {
+			return this.images.slice(0, this.indexOfActualChunk)
 		}
 	},
 	components: {
 		Item
+	},
+	methods: {
+		loadMore: function () {
+			this.currentChunk++
+		}
 	}
 }
 </script>
