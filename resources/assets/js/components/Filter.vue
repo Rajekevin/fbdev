6d210f0d0547e195690c3fcae9d7f84340a3b749
Aktane <antoine.renault.mmi@gmail.com>
2017-01-04 11:53:59 +0100

    Add conditionnal viewing and some style on the filter button

--- resources/assets/js/components/Filter.vue
+++ resources/assets/js/components/Filter.vue
@@ -1,44 +1,44 @@
 <template>
-	<div class="filter">
-	  	<div @click="filterMoreLike()">Plus aimé</div>
-	  	<div @click="filterLessLike()">Moins aimé</div>
-	  	<div @click="filterRandom()">Random</div>
-	  	<div @click="filterMoreRecent()">Plus récent</div>
-	  	<div @click="filterLessRecent()">Moin récent</div>
-	  	<div @click="filterDescTitle()">A - Z</div>
-	  	<div @click="filterAscTitle()">Z - A</div>
+	<div class="filter no-select">
+		<div @click="filterRandom()">Random</div>
+		<div v-if="state == 'lessLiked'" @click="filterMoreLike()">Plus aimé</div>
+		<div v-else @click="filterLessLike()">Moins aimé</div>
+		<div v-if="state == 'lessRecent'" @click="filterMoreRecent()">Plus récent</div>
+		<div v-else @click="filterLessRecent()">Moins récent</div>
+		<div v-if="state == 'titleAscendant'" @click="filterDescTitle()">A - Z</div>
+		<div v-else @click="filterAscTitle()">Z - A</div>
 	</div>
 </template>
 
 <script>
-export default {
-	methods: {
-		filterMoreLike: function () {
-			this.$store.commit('setFilterState', 'moreLiked')
+	export default {
+		methods: {
+			filterMoreLike: function () {
+				this.$store.commit('setFilterState', 'moreLiked')
+			},
+			filterLessLike: function () {
+				this.$store.commit('setFilterState', 'lessLiked')
+			},
+			filterRandom: function () {
+				this.$store.commit('setFilterState', 'random')
+			},
+			filterMoreRecent: function () {
+				this.$store.commit('setFilterState', 'moreRecent')
+			},
+			filterLessRecent: function () {
+				this.$store.commit('setFilterState', 'lessRecent')
+			},
+			filterAscTitle: function () {
+				this.$store.commit('setFilterState', 'titleAscendant')
+			},
+			filterDescTitle: function () {
+				this.$store.commit('setFilterState', 'titleDescendant')
+			}
 		},
-		filterLessLike: function () {
-			this.$store.commit('setFilterState', 'lessLiked')
-		},
-		filterRandom: function () {
-			this.$store.commit('setFilterState', 'random')
-		},
-		filterMoreRecent: function () {
-			this.$store.commit('setFilterState', 'moreRecent')
-		},
-		filterLessRecent: function () {
-			this.$store.commit('setFilterState', 'lessRecent')
-		},
-		filterAscTitle: function () {
-			this.$store.commit('setFilterState', 'titleAscendant')
-		},
-		filterDescTitle: function () {
-			this.$store.commit('setFilterState', 'titleDescendant')
-		}
-	},
-	computed: {
-		state: function () {
-			return this.$store.state.filterState
+		computed: {
+			state: function () {
+				return this.$store.state.filterState
+			}
 		}
 	}
-}
 </script>
