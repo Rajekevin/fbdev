2deccdab86e049611e4feab49b5c74e774e0ff6b
Myriam Haas <Myriam Haas>
2017-01-04 18:13:23 +0100

    Css for participate page, left container

--- resources/views/frontend/html/pages/participate/albums.blade.php
+++ resources/views/frontend/html/pages/participate/albums.blade.php
@@ -1,4 +1,8 @@
 <div class="participate_block_left">
+    <p>Télécharger une photo depuis mon ordinateur</p>
+    <input type="file" name="image_upload" />
+    <p class="bold">ou</p>
+    <p>depuis mes albums Facebook</p>
     <ul>
         <li>Album 1</li>
         <li>Album 2</li>
